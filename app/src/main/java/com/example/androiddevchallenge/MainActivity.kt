a07bb822ff7178e10e96040ae97c177734e77658
RickyHal <1107786871@qq.com>
2021-03-01 19:23:54 +0800

    complete!

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,33 +16,33 @@
 package com.example.androiddevchallenge
 
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.entities.Pet
 import com.example.androiddevchallenge.ui.public.AppBar
@@ -114,9 +114,9 @@ class MainActivity : AppCompatActivity() {
         val dataSource = getPets()
         setContent {
             MyTheme {
-                MyApp(dataSource){
-                    val intent = Intent(this,DetailActivity::class.java)
-                    intent.putExtra(DetailActivity.KEY_PET,it)
+                MyApp(dataSource) {
+                    val intent = Intent(this, DetailActivity::class.java)
+                    intent.putExtra(DetailActivity.KEY_PET, it)
                     startActivity(intent)
                 }
             }
@@ -138,11 +138,11 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp(dataSource: List<Pet>,onItemClick:(Pet)->Unit) {
+fun MyApp(dataSource: List<Pet>, onItemClick: (Pet) -> Unit) {
     LazyColumn {
         item { AppBar("Welcome to Cat House") }
         items(dataSource) { pet ->
-            Row(Modifier.clickable { onItemClick(pet)}) {
+            Row(Modifier.clickable { onItemClick(pet) }) {
                 Image(painter = painterResource(id = pet.photo),
                     contentDescription = null,
                     modifier = Modifier
@@ -195,18 +195,3 @@ fun MyApp(dataSource: List<Pet>,onItemClick:(Pet)->Unit) {
         }
     }
 }
-//@Preview("Light Theme", widthDp = 360, heightDp = 640)
-//@Composable
-//fun LightPreview() {
-//    MyTheme {
-//        MyApp()
-//    }
-//}
-//
-//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-//@Composable
-//fun DarkPreview() {
-//    MyTheme(darkTheme = true) {
-//        MyApp()
-//    }
-//}
